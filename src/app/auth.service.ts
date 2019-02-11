b58faedcb6f9d986f93c095be3fea04765d7bec3
Caleb Nordgren <cdn841@gmail.com>
2019-02-11 11:30:40 -0600

    added login page

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './users';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
 
-  constructor() { }
+export class AuthService {
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    let l = window.location;
+    this.url = `${l.protocol}://${l.hostname}/api/auth/`;
+  }
+
+  register (user: User): Observable<User> {
+    return this.http.post<User>(this.url + 'register', user, httpOptions);
+  }
+
+  logIn(user: User): Observable<User> {
+    return this.http.post<User>(this.url + 'login', user, httpOptions);
+  }
+
+  logOut(user: User) {
+    return this.http.get<User>(this.url + 'logout')
+  }
+
 }
